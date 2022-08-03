0135d35eced7c7d9863069b2d9270c5be42a1d8c
wyk172899 <wyk172899@gmail.com>
2022-08-03 11:54:10 +0900

    commit

--- app/src/main/java/org/devele/alermi/MainActivity.java
+++ app/src/main/java/org/devele/alermi/MainActivity.java
@@ -1,6 +1,13 @@
 package org.devele.alermi;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
---
+++ app/src/main/java/org/devele/alermi/RegisterMentActivity.java
@@ -0,0 +1,14 @@
+package org.devele.alermi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterMentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_ment);
+    }
+}
